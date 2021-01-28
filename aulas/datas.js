fd9d416a82f2714b3fcc561bdd4b39be2a454b6b
rafael-conrado <rafael070893@hotmail.com>
2021-01-28 19:31:20 -0300

    switch

--- aulas/datas.js
+++ aulas/datas.js
@@ -18,7 +18,7 @@ console.log('ms', data.getMilliseconds());
 console.log('Dia semana', data.getDay());
 
 */
-function inserirZero(num) {
+/* function inserirZero(num) {
     return num >= 10 ? num : `0${num}`
 }
 
@@ -30,12 +30,69 @@ function formataData(data) {
     const min = inserirZero(data.getMinutes());
     const seg = inserirZero(data.getSeconds());
     return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`
-}
+} */
 
-const hoje = new Date();
+/* const hoje = new Date();
 const dataBtasil = formataData(hoje)
-console.log(formataData(hoje))
+console.log(formataData(hoje)) */
 
 
-const diasemana = hoje.getDay();
-console.log(diasemana)
+/* const diaSemana = hoje.getDay();
+
+const data = new Date('1993-08-07 00:00:00')
+let diaDaSemana = data.getDay();
+let diaDaSemanaTexto;
+
+switch (diaDaSemana) {
+    case 0:
+        diaDaSemanaTexto = 'Domingo';
+        break;
+    case 1:
+        diaDaSemanaTexto = "Segunda";
+        break;
+    case 2:
+        diaDaSemanaTexto = "Terça";
+        break;
+    case 3:
+        diaDaSemanaTexto = "Quarta";
+        break;
+    case 4:
+        diaDaSemanaTexto = "Quinta";
+        break;
+    case 5:
+        diaDaSemanaTexto = "Sexta";
+        break;
+    case 6:
+        diaDaSemanaTexto = "Sábado";
+        break;
+} 
+console.log(diaDaSemana, diaDaSemanaTexto)
+console.log(diaSemana) */
+
+function getDiaDaSemanaTexto(diasemana) {
+    let diaDaSemanaTexto;
+    switch (diaDaSemana) {
+        case 0:
+            diaDaSemanaTexto = 'Domingo';
+            return diaDaSemanaTexto;
+        case 1:
+            diaDaSemanaTexto = "Segunda";
+            return diaDaSemanaTexto;
+        case 2:
+            diaDaSemanaTexto = "Terça";
+            return diaDaSemanaTexto;
+        case 3:
+            diaDaSemanaTexto = "Quarta";
+            return diaDaSemanaTexto;
+        case 4:
+            diaDaSemanaTexto = "Quinta";
+            return diaDaSemanaTexto;
+        case 5:
+            diaDaSemanaTexto = "Sexta";
+            return diaDaSemanaTexto;
+        case 6:
+            diaDaSemanaTexto = "Sábado";
+            return diaDaSemanaTexto;
+    } 
+    
+}
